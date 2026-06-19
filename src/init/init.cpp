76d28ffabfdbe076137fa847d5234d6484b17c6a
oxmmty <oxmmty@gmail.com>
2026-06-19 13:21:20 +0100

    replace removed boost::filesystem basename/extension free functions for boost 1.86 windows build

--- src/init/init.cpp
+++ src/init/init.cpp
@@ -555,7 +555,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     std::string strWalletFileName = GetArg("-wallet", "wallet.dat");
 
     // strWalletFileName must be a plain filename without a directory
-    if (strWalletFileName != fs::basename(strWalletFileName) + fs::extension(strWalletFileName))
+    if (strWalletFileName != fs::path(strWalletFileName).filename().string())
         return InitError(strprintf(_("Wallet %s resides outside data directory %s."), strWalletFileName.c_str(), strDataDir.c_str()));
 
     if (mapArgs.count("-bip44key") && fs::exists(GetDataDir() / strWalletFileName))
