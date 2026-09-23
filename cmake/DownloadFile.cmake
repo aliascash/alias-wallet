0007db3bb8d41cb390d8bdf698340b96aa95166b
oxmmty <oxmmty@gmail.com>
2026-09-23 09:35:25 +0900

    get the Tor expert bundle from the permanent archive and fail the build when a download fails

--- cmake/DownloadFile.cmake
+++ cmake/DownloadFile.cmake
@@ -7,6 +7,15 @@ function(download_file url filename hash_type hash)
         file(DOWNLOAD ${url} ${filename}
                 TIMEOUT 60  # seconds
                 EXPECTED_HASH ${hash_type}=${hash}
-                TLS_VERIFY ON)
+                TLS_VERIFY ON
+                STATUS downloadStatus)
+        list(GET downloadStatus 0 downloadCode)
+        if (NOT downloadCode EQUAL 0)
+            list(GET downloadStatus 1 downloadError)
+            # Remove the partial file, otherwise the next configure run sees it
+            # and skips the download.
+            file(REMOVE ${filename})
+            message(FATAL_ERROR "Download failed: ${url}\n  ${downloadError}")
+        endif ()
     endif ()
 endfunction(download_file)
